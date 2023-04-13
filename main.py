489adcbd4e553d06808a0a9dfbb970de3118b793
absenth <1115138+absenth@users.noreply.github.com>
2023-04-13 12:08:04 -0500

    improving the labling, and throwing away frames while others are being processed

--- main.py
+++ main.py
@@ -22,13 +22,20 @@ def main():
     stream_url = f'rtsp://{stream_user}:{stream_pass}@{stream_server}:{stream_port}/Streaming/Channels/{stream_channel}'
 
     cap = cv2.VideoCapture(stream_url)
-    model = YOLO('yolov8s.pt')
-    desired_class = 0
+    model = YOLO('yolov8l.pt')
+    class_names = model.names
+    desired_class = 'person'
 
+    processing_frame = False
     while True:
         ret, frame = cap.read()
         if not ret:
             continue  # skip to the next iteration if there was an issue reading the frame
+
+        if processing_frame:
+            continue
+
+        processing_frame = True
 
         results = model(frame, device='mps')
         result = results[0]
@@ -37,17 +44,19 @@ def main():
         confidences = np.array(result.boxes.conf.cpu(), dtype='float')
         for bbox, cls, confidence in zip(bboxes, classes, confidences):
             (x, y, x2, y2) = bbox
-            if cls != desired_class:
+            if cls != 0:
                 continue
             if confidence < 0.50:
                 continue
             cv2.rectangle(frame, (x, y), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(frame, str(cls), (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(frame, class_names[cls], (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         cv2.imshow('IMG', frame)
         key = cv2.waitKey(1)
         if key == 27:
             break
+
+        processing_frame = False
 
     cap.release()
     cv2.destroyAllWindows()
