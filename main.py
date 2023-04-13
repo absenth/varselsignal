3fa9b1945fb919d90f499ee855fbf15fa4d16d53
absenth <1115138+absenth@users.noreply.github.com>
2023-04-13 11:25:18 -0500

    Working Person Detector, optimized for Apple Silicon Macs

--- main.py
+++ main.py
@@ -1,49 +1,57 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-
-image = plt.imread('corner-still.png')
-
-classes = None
-with open('coco.names', 'r') as f:
-    classes = [line.strip() for line in f.readlines()]
-
-net = cv2.dnn.readNet('yolov3-tiny.weights', 'yolov3-tiny.cfg')
-net.setInput(cv2.dnn.blobFromImage(image, 0.00392, (416,416), (0,0,0), True, crop=False))
-layer_names = net.getLayerNames()
-output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-outs = net.forward(output_layers)
+import os
+import torch
+from dotenv import load_dotenv
+from ultralytics import YOLO
 
 
-class_ids = []
-confidences = []
-boxes = []
-Width = image.shape[1]
-Height = image.shape[0]
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-        if confidence > 0.1:
-            center_x = int(detection[0] * Width)
-            center_y = int(detection[1] * Height)
-            w = int(detection[2] * Width)
-            h = int(detection[3] * Height)
-            x = center_x - w / 2
-            y = center_y - h / 2
-            class_ids.append(class_id)
-            confidences.append(float(confidence))
-            boxes.append([x, y, w, h])
+def get_creds():
+    load_dotenv()
+    stream_server = os.getenv('STREAM_SERVER')
+    stream_user = os.getenv('STREAM_USER')
+    stream_pass = os.getenv('STREAM_PASS')
+    stream_port = os.getenv('STREAM_PORT')
+    stream_channel = os.getenv('STREAM_CHANNEL')
+
+    return stream_server, stream_user, stream_pass, stream_port, stream_channel
 
 
-indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.1, 0.1)
-#check if is people detection
-for i in indices:
-    i = i[0]
-    box = boxes[i]
-    if class_ids[i]==0:
-        label = str(classes[class_id]) 
-        cv2.rectangle(image, (round(box[0]),round(box[1])), (round(box[0]+box[2]),round(box[1]+box[3])), (0, 0, 0), 2)
-        cv2.putText(image, label, (round(box[0])-10,round(box[1])-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-plt.imwrite('corner_still-yolo.png')
+def main():
+    stream_server, stream_user, stream_pass, stream_port, stream_channel = get_creds()
+    stream_url = f'rtsp://{stream_user}:{stream_pass}@{stream_server}:{stream_port}/Streaming/Channels/{stream_channel}'
+
+    cap = cv2.VideoCapture(stream_url)
+    model = YOLO('yolov8s.pt')
+    desired_class = 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            continue  # skip to the next iteration if there was an issue reading the frame
+
+        results = model(frame, device='mps')
+        result = results[0]
+        bboxes = np.array(result.boxes.xyxy.cpu(), dtype='int')
+        classes = np.array(result.boxes.cls.cpu(), dtype='int')
+        confidences = np.array(result.boxes.conf.cpu(), dtype='float')
+        for bbox, cls, confidence in zip(bboxes, classes, confidences):
+            (x, y, x2, y2) = bbox
+            if cls != desired_class:
+                continue
+            if confidence < 0.50:
+                continue
+            cv2.rectangle(frame, (x, y), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(frame, str(cls), (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        cv2.imshow('IMG', frame)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
